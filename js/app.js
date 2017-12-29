d90aeadcbb81708116372360ea8cd0a2b05a96f5
wendytamara <anacarlavegam@gmail.com>
2017-12-29 14:13:08 -0500

    añadiendo funcion de codigo aleatorio js

--- js/app.js
+++ js/app.js
@@ -10,23 +10,28 @@ function begin(){
 
  },1000 );
 
- // funcion para validar el numero de tlf
+ // nombrando variables reutilizables
     var $number = $("#icon_telephone");
     var $boton = $(".btn-next");
 
+// nombrando eventos y funciones
     $number.on("keyup",validCelphoneNumber);
+    $boton.on("click", randomCode);
 
+// funcion para validar el numero de tlf de 10 dgts
     function validCelphoneNumber(){
       if ($number.val().length === 10) {
-          $boton.removeClass("disabled");
+        $boton.removeClass("disabled");
       }
       else {
         $boton.addClass("disabled");
       }
     }
 
-
-
+    // funcion para crear el alert con el codigo aleatorio
+    function randomCode(){
+      alert("Tu codigo: " + "LAB "  + Math.floor((Math.random() * 1000) + 1));
+    }
 
 
 
