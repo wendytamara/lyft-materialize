27ad693140cc17e60ab3701a5d741101eccd6eee
wendytamara <anacarlavegam@gmail.com>
2017-12-29 12:38:13 -0500

    añadiendo funcionalidad en js

--- js/app.js
+++ js/app.js
@@ -1,5 +1,35 @@
-$("document").ready(function(){
+function begin(){
+  // funccion para inicializar el formulario materialize
   $('select').material_select();
+// funcion para realizar el splash
+  $("main").hide();
+
+  setTimeout(function() {
+   $("section").fadeOut();
+   $("main").show();
+
+ },1000 );
+
+ // funcion para validar el numero de tlf
+    var $number = $("#icon_telephone");
+    var $boton = $(".btn-next");
+
+    $number.on("keyup",validCelphoneNumber);
+
+    function validCelphoneNumber(){
+      if ($number.val().length === 10) {
+          $boton.removeClass("disabled");
+      }
+      else {
+        $boton.addClass("disabled");
+      }
+    }
 
 
-});
+
+
+
+
+ }
+
+$(document).ready(begin);
